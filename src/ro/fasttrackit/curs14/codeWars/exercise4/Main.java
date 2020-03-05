afe8d22b032448f0c9f47ce0edcbb450bfb543f3
Varadi <mark.varadi03@gmail.com>
2020-03-05 18:49:10 +0200

    modified project

--- src/ro/fasttrackit/curs14/codeWars/exercise4/Main.java
+++
@@ -1,8 +0,0 @@
-package ro.fasttrackit.curs14.codeWars.exercise4;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Scramblies.scramble2("cedewaraaossoqqyt", "codewars"));
-
-    }
-}
